a21407048567a09ad4dd74a29e37e311153665a5
tambakdev <tambakdev@gmail.com>
2020-09-15 08:38:56 +0700

    readme

--- tailwind.js
+++ tailwind.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "spin-slow": "spin 10s linear infinite",
+        "spin-slow": "spin 20s linear infinite",
       },
     },
   },
