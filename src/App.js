bfa5c5c0d8a14d69594983ebcbc25c7048e2c625
tambakdev <tambakdev@gmail.com>
2020-09-15 08:33:02 +0700

    bismillah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="h-screen content-center">
+      <header className="bg-gray-900 h-full text-center text-white pt-24">
+        <img src={logo} className="h-64 w-64 mx-auto animate-spin-slow" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span className="font-medium text-2xl">Edit</span>
+          <code className="font-thin"> src/App.js</code>{" "}
+          <span className="font-medium text-2xl">and save to reload.</span>
         </p>
         <a
-          className="App-link"
+          className="text-blue-300 underline text-2xl"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
+        <p className="mt-20 flex justify-center">
+          <a
+            className="text-blue-300 underline text-2xl hover:text-blue-400 mx-3"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            React
+          </a>
+
+          <a
+            className="text-blue-300 underline text-2xl hover:text-blue-400 mx-3"
+            href="https://eslint.org/"
+            target="_blank"
+            rel="noopener noreferrer">
+            ESlint
+          </a>
+
+          <a
+            className="text-blue-300 underline text-2xl hover:text-blue-400 mx-3"
+            href="https://github.com/airbnb/javascript"
+            target="_blank"
+            rel="noopener noreferrer">
+            Airbnb Style
+          </a>
+
+          <a
+            className="text-blue-300 underline text-2xl hover:text-blue-400 mx-3"
+            href="https://tailwindcss.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+            TailwindCSS
+          </a>
+        </p>
       </header>
     </div>
   );
---
+++ tailwind.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 10s linear infinite",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
