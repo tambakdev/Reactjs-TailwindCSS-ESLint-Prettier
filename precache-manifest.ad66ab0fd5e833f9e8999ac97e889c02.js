17fd2fae0b137990114f4296ab4f4f1d3e542ab6
tambakdev <tambakdev@gmail.com>
2020-09-15 08:54:05 +0700

    Updates

---
+++ precache-manifest.ad66ab0fd5e833f9e8999ac97e889c02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472d51318f8f72ec09d2caa37e5d3721",
+    "url": "/Reactjs-TailwindCSS-ESLint-Prettier/index.html"
+  },
+  {
+    "revision": "4232597cdc0973ccda4f",
+    "url": "/Reactjs-TailwindCSS-ESLint-Prettier/static/css/main.adadb904.chunk.css"
+  },
+  {
+    "revision": "b5a50b56818f0d0368b1",
+    "url": "/Reactjs-TailwindCSS-ESLint-Prettier/static/js/2.0c90ca15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactjs-TailwindCSS-ESLint-Prettier/static/js/2.0c90ca15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4232597cdc0973ccda4f",
+    "url": "/Reactjs-TailwindCSS-ESLint-Prettier/static/js/main.338a458d.chunk.js"
+  },
+  {
+    "revision": "309b2ce1bc2c605080d6",
+    "url": "/Reactjs-TailwindCSS-ESLint-Prettier/static/js/runtime-main.9fd36086.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Reactjs-TailwindCSS-ESLint-Prettier/static/media/logo.5d5d9eef.svg"
+  }
+]);
